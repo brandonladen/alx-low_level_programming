2c5407b72c7a678f1e26efc490896683bdedc251
brandonladen <brandonladen486@gmail.com>
2023-02-16 05:39:30 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using uts function instead of printf
+ * Return: 0
+ */
+int main(void)
+{
+puts('"Programming is like building a multilingual puzzle');
+return (0);
+}
+
