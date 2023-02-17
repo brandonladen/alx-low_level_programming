0b48e4b8a001c4434fbadc0ef522985a73d197d4
brandonladen <brandonladen486@gmail.com>
2023-02-17 09:51:02 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - A program to print alphabets
+ * Return: 0
+ */
+int main(void)
+{
+char *alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$\n";
+size_t i;
+for (i = 0; i < strlen(alphabets); i++)
+{
+putchar(alphabets[i]);
+}
+return (0);
+}
