f05100b5992056d3362b649b1ec5b80cf08f01cc
brandonladen <brandonladen486@gmail.com>
2023-02-17 10:32:24 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
