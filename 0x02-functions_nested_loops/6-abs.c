6fe3202516d1fd03638038cde7f6ece67afc46f3
brandonladen <brandonladen486@gmail.com>
2023-02-21 15:44:03 -0800

    Absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - A program that prints the absolute value of a number
+ * @n: The number to be used
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (n * -1);
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
