c2b16611af23cf45ce844a97d0c4b8fd4b9d2410
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-02-21 21:25:22 +0300

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,12 +13,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-	_putchar(45);
-	return (-1);	
+	_putchar(48);
+	return (0);	
 	}
 	else
 	{
-	_putchar(48);
-	return (0);	
+	_putchar(45);
+	return (-1);	
 	}
 }
