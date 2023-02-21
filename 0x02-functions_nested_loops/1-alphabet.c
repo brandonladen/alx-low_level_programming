80b9a8492b81e08ed6f2d8159f99cb26387fa050
brandonladen <brandonladen486@gmail.com>
2023-02-20 23:12:31 -0800

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
