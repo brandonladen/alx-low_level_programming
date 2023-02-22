e6b2d89efcaf09419be5543adb78967d4a0c3d5e
brandonladen <brandonladen486@gmail.com>
2023-02-21 16:33:21 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - A program that adds two integers
+ * @a: the first number
+ * @b: the second number
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
