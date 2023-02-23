72ff6885fa518a1e9891910b91869bb708d5bb2f
brandonladen <brandonladen486@gmail.com>
2023-02-23 12:04:38 -0800

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,23 +1,44 @@
 #include "main.h"
+
 /**
- * print_triangle - Prints triangle
- * @size: the size of the triangle
+ * print_triangle - function declaration
+ * Description: prints a triangle
+ * @size: triangle height
  * Return: 0
  */
+
+void print_triangle(int size);
+
+/**
+ * print_triangle - function definition
+ * Description: prints right angle triangle
+ * @size: triangle height in dashes
+ * Return: 0
+ */
+
 void print_triangle(int size)
 {
-	int i, j, k;
+	int range;
+	int space;
+	int hash;
 
-	for (i = 1; i <=size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j <= size - 1; j++)
-		{
-			_putchar(' ');
-		}
-		for (k = 1; k <= i; k++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (range = 1; range <= size; range++)
+		{
+			for (space = size - range; space > 0; space--)
+			{
+				_putchar(' ');
+			}
+			for (hash = 0; hash < range; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
