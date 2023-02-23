1af171170560b8f3e00404dc520c41c8071dfdbd
brandonladen <brandonladen486@gmail.com>
2023-02-23 03:37:11 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,31 +5,31 @@
  */
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-			printf(" ");
-		}
-		else if (i % 5 != 0 && i % 3 == 0)
-		{
-			printf("Fizz");
-			printf(" ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-			printf(" ");
-		}
-		else
-		{
-			printf("%d", i);
-			printf(" ");
-		}
-	}
-	printf("\n");
-	return (0);
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
 }
