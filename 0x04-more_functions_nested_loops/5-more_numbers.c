3290f6a0963df1923a19007ba0b0fbc565e36e30
brandonladen <brandonladen486@gmail.com>
2023-02-22 22:37:02 -0800

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - Prints from 0 to 14
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int counter = 0;
+
+	do {
+		for (i = 0; i < 15; i++)
+		{
+			if (i < 10)
+			{
+				_putchar(i + '0');
+			}
+			else
+			{
+				_putchar('1');
+				_putchar(i - 10 + '0');
+			}
+		}
+		_putchar('\n');
+		counter++;
+	} while (counter < 11);
+}
