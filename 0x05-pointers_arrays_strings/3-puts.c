6e2bc8ba10746d9a27ea8e5c0ec36adb9518cf96
brandonladen <brandonladen486@gmail.com>
2023-02-26 23:01:26 -0800

    string manipulation

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - A function thta prints a string
+ * @str: the parameter
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
