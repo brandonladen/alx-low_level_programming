0af8ba35c7dceb8c173ea9080431ca4bcb9d2771
brandonladen <brandonladen486@gmail.com>
2023-02-26 21:03:03 -0800

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - A function that takes a pointer and updates it to 98
+ * @n: parameter to recieve argument
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_NAME_H
+#define HEADER_NAME_H
+void reset_to_98(int *n);
+#endif /* HEADER_NAME_H */
