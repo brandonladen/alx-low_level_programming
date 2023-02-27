5900c5aebedddcdd703284fdae2ff990fe24b6e6
brandonladen <brandonladen486@gmail.com>
2023-02-26 22:18:54 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - A program that returns the length of a string
+ * @s: parameter
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
