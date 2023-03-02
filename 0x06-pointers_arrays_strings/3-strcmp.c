ac6c11b54196712a448cf0167e8673e042f057d3
brandonladen <brandonladen486@gmail.com>
2023-03-02 01:55:39 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strcmp - Compares two strings
+* @s1: input
+* @s2: input
+* Return: dest
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (0);
+}
