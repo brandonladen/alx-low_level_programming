6b631a9273315e28831da04a974f6ed99f4ac3e8
brandonladen <brandonladen486@gmail.com>
2023-03-06 12:11:32 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - A function that locates a substring
+ * @haystack: This is the main C string to be scanned
+ * @needle: This is the small string to be searched with-in haystack string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *ret;
+
+	ret = strstr(haystack, needle);
+	return (ret);
+}
