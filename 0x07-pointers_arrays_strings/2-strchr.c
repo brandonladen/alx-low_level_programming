daf4eda9ffdcde9fe6aa426b5c38bf7ecd819f2c
brandonladen <brandonladen486@gmail.com>
2023-03-06 10:20:54 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - A function that locates a character in a string
+ * @s: pointer
+ * @c: charater to be copied
+ */
+char *_strchr(char *s, char c)
+{
+	char *ret;
+
+	ret = strchr(s, c);
+	return (ret);
+}
