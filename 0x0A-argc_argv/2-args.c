6b7fc76e9dfb0a889b77e5b584a4bd291b72868e
brandonladen <brandonladen486@gmail.com>
2023-03-10 16:19:43 -0800

    taks 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints number of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+		printf("\n");
+	}
+	return (0);
+}
