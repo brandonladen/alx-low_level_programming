129a432a11430a325d5eeb0c8049b3241f487b0d
brandonladen <brandonladen486@gmail.com>
2023-03-16 08:25:33 -0700

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,179 +1,118 @@
 #include "main.h"
-
+#include <stdio.h>
 #include <stdlib.h>
 
-#include <stdio.h>
-
-
-
-#define ERR_MSG "Error"
-
-
-
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- * Return: 0 if a non-digit is found, 1 otherwise
+ * _isdigit - checks if a character is a digit
+ * @c: the character to check
+ *
+ * Return: 1 if the character is a digit, 0 otherwise
  */
-
-int is_digit(char *s)
-
+int _isdigit(int c)
 {
-
-	int i = 0;
-
-
-
-	while (s[i])
-
-	{
-
-		if (s[i] < '0' || s[i] > '9')
-
-			return (0);
-
-		i++;
-
-	}
-
-	return (1);
-
+	return (c >= '0' && c <= '9');
 }
 
-
-
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- * Return: the length of the string
+ * print_error - prints an error message and exits
  */
-
-int _strlen(char *s)
-
+void print_error(void)
 {
-
-	int i = 0;
-
-
-
-	while (s[i] != '\0')
-
-	{
-
-		i++;
-
-	}
-
-	return (i);
-
-}
-
-
-/**
- * errors - handles errors for main
- */
-
-void errors(void)
-
-{
-
 	printf("Error\n");
-
 	exit(98);
-
 }
 
+/**
+ * multiply - multiplies two positive numbers represented as arrays of digits
+ * @num1: the first number
+ * @num2: the second number
+ * @len1: the length of the first number
+ * @len2: the length of the second number
+ *
+ * Return: a pointer to the result of the multiplication
+ */
+char *multiply(char *num1, char *num2, int len1, int len2)
+{
+	int i, j, k, carry, n;
+	char *result;
 
+	result = malloc(len1 + len2);
+	if (result == NULL)
+		return (NULL);
+
+	/* initialize result */
+	for (i = 0; i < len1 + len2; i++)
+		result[i] = '0';
+
+	/* multiply each digit of num1 with each digit of num2 */
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n = (num1[i] - '0') * (num2[j] - '0') +
+				(result[i + j + 1] - '0') + carry;
+			result[i + j + 1] = (n % 10) + '0';
+			carry = n / 10;
+		}
+		result[i] += carry;
+	}
+
+	/* remove leading zeroes */
+	k = 0;
+	while (k < len1 + len2 && result[k] == '0')
+		k++;
+	if (k == len1 + len2)
+		k--;
+
+	/* shift the result to the left */
+	for (i = 0; i <= len1 + len2 - k; i++)
+		result[i] = result[i + k];
+
+	return (result);
+}
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: always 0 (Success)
+ * main - entry point
+ * @argc: the number of arguments
+ * @argv: an array of pointers to the arguments
+ *
+ * Return: 0 on success, 98 on failure
  */
-
-int main(int argc, char *argv[])
-
+int main(int argc, char **argv)
 {
+	char *num1, *num2, *result;
+	int len1, len2;
 
-	char *s1, *s2;
+	if (argc != 3)
+		print_error();
 
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	num1 = argv[1];
+	num2 = argv[2];
 
-
-
-	s1 = argv[1], s2 = argv[2];
-
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-
-		errors();
-
-	len1 = _strlen(s1);
-
-	len2 = _strlen(s2);
-
-	len = len1 + len2 + 1;
-
-	result = malloc(sizeof(int) * len);
-
-	if (!result)
-
-		return (1);
-
-	for (i = 0; i <= len1 + len2; i++)
-
-		result[i] = 0;
-
-	for (len1 = len1 - 1; len1 >= 0; len1--)
-
+	len1 = 0;
+	while (num1[len1])
 	{
-
-		digit1 = s1[len1] - '0';
-
-		carry = 0;
-
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-
-		{
-
-			digit2 = s2[len2] - '0';
-
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-
-			result[len1 + len2 + 1] = carry % 10;
-
-			carry /= 10;
-
-		}
-
-		if (carry > 0)
-
-			result[len1 + len2 + 1] += carry;
-
+		if (!_isdigit(num1[len1]))
+			print_error();
+		len1++;
 	}
 
-	for (i = 0; i < len - 1; i++)
-
+	len2 = 0;
+	while (num2[len2])
 	{
-
-		if (result[i])
-
-			a = 1;
-
-		if (a)
-
-			_putchar(result[i] + '0');
-
+		if (!_isdigit(num2[len2]))
+			print_error();
+		len2++;
 	}
 
-	if (!a)
+	result = multiply(num1, num2, len1, len2);
+	if (result == NULL)
+		print_error();
 
-		_putchar('0');
-
-	_putchar('\n');
+	printf("%s\n", result);
 
 	free(result);
 
 	return (0);
-
 }
