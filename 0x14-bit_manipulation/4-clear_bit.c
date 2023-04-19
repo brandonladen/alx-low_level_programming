733960357847d84e92d7f811bdb7008392f4f371
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-04-19 21:58:48 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,1 +1,21 @@
-
+#include "main.h"
+/**
+ * clear_bit - A function that sets the value of a bit to 0 at a given index
+ * @n: A pointer to a number
+ * index: Where to set the bit to 0
+ * Return: 1 Succees or -1 for error
+ * Author - BRANDON ODHIAMBO
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+  if (index > 64)
+  {
+    return (-1);
+  }
+  if ((*n >> index) & 1)
+  {
+    *n -= 1 << index;
+    return (1);
+  }
+  return (1);
+}
