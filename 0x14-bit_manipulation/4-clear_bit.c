db4d6de84cb0409f758e61a51a5defec2903ac5a
brandonladen <brandonladen486@gmail.com>
2023-04-19 12:26:00 -0700

    updated file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,14 +8,12 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	if ((*n >> index) & 1)
-	{
-		*n -= 1 << index;
-		return (1);
-	}
+
+	*n &= ~(1UL << index);
+
 	return (1);
 }
