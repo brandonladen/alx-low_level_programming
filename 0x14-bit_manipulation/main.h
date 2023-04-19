f0e181d47606ae6ebf2eeb167a77297426ea8702
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-04-19 22:32:29 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,7 @@ int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_endianness(void);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif /* NAME_H */
