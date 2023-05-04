d9a2cd19cd1d29bcd0babb0322e7310756059077
brandonladen <brandonladen486@gmail.com>
2023-05-04 09:27:07 -0700

    corrected

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,28 +8,25 @@
  */
 void print_binary(unsigned long int n)
 {
-	char b[33];
-	int i;
+	unsigned int mask;
+	int flag;
 
-	for (i = 0; i < 32; i++)
-		b[i] = '0';
-	b[i] = '\0';
-
-	i = 31;
-	while (n > 0)
+	mask = 1 << 31;
+	flag = 0;
+	while (mask > 0)
 	{
-		if (n & 1)
-			b[i] = '1';
-		n >>= 1;
-		i--;
+		if (n & mask)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag)
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
 	}
-	i = 0;
-	while (b[i] == '0' && b[i + 1] != '\0')
-		i++;
-	while (b[i] != '\0')
-	{
-		_putchar(b[i]);
-		i++;
-	}
+	if (n == 0)
+		_putchar('0');
 	_putchar('\n');
 }
