43cca7c875d720a33c6db26e770444095e592d75
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-05-08 13:04:38 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,24 +19,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	b = malloc(sizeof(char) * letters);
 	if (b == NULL)
-		free(b);
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
 	t = read(fd, b, letters);
-	if (t == -1)
-	{
-		free(b);
-		close(fd);
-		return (0);
-	}
 	w = write(STDOUT_FILENO, b, t);
-	if (w == -1 || w != t)
+	if (fd == -1 || t == -1 || w == -1 || w != t)
 	{
 		free(b);
-		close(fd);
 		return (0);
 	}
 
