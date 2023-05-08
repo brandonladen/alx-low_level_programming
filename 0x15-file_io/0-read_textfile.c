4eabe0b66c1538489f7f278792fd89ae78d2ad5e
brandonladen <brandonladen486@gmail.com>
2023-05-08 02:34:42 -0700

    added a requirement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,6 +15,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w;
 	ssize_t t;
 
+	if (filename == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
