8d1c75512f744f12c692d429254acee33aad7831
brandonladen <brandonladen486@gmail.com>
2023-06-15 04:21:47 -0700

    created 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - a function that frees a dlistint_t list.
+ * @head: A pointer to the header of the list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (temp != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
