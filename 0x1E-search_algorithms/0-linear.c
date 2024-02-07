8906fcd88ce35360f12e334c2c190a9abd0dc797
brandonladen <brandonladen486@gmail.com>
2024-02-07 13:56:23 +0300

    solved betty error 'parentheses are required on a return statement'

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,16 +15,16 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 
 	}
-	return -1;
+	return (-1);
 
 }
